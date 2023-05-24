03e9c653afbb2bd8fbfd8b46dcc007919bb3e1ec
someoneisyes <naratgutz@gmail.com>
2023-05-24 11:50:41 +0300

    add many many things

---
+++ src/test_jabalizer.jl
@@ -0,0 +1,10 @@
+using Jabalizer
+
+circuit = [("H",["1"]),("CNOT",["1","2"]),("CNOT",["1","3"])]
+
+icm_circuit = Jabalizer.compile(circuit, 3, [""])
+
+println(icm_circuit)
+
+Jabalizer.save_circuit_to_cirq_json(icm_circuit[1], "./src/circuit")
+
--- src/test_stim.py
+++ src/test_stim.py
@@ -1,4 +1,6 @@
 import stim
+import cirq
+import stimcirq
 
 circuit = stim.Circuit("""
 H 0
@@ -7,3 +9,11 @@ M 0 1
 """)
 
 print(circuit.compile_sampler().sample(10))
+
+f = open("./src/circuit")
+
+json_string = f.read()
+
+new_circuit = cirq.read_json(json_text=json_string)
+
+print(new_circuit)
