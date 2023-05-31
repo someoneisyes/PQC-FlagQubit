e719c96fc81b1577ef0e6a1825b55dd901acf999
someoneisyes <naratgutz@gmail.com>
2023-05-31 14:05:08 +0300

    The test circuit is now the toffoli gate

--- src/test_jabalizer.jl
+++ src/test_jabalizer.jl
@@ -1,8 +1,24 @@
 using Jabalizer
 
-circuit = [("H",["1"]),("CNOT",["1","2"])]
+circuit = [
+    ("H",["3"]),
+    ("CNOT",["2","3"]),
+    ("T^-1",["3"]),
+    ("CNOT",["1","3"]),
+    ("T",["3"]),
+    ("CNOT",["2","3"]),
+    ("T^-1",["3"]),
+    ("CNOT",["1","3"]),
+    ("T",["2"]),
+    ("T",["3"]),
+    ("CNOT",["1","2"]),
+    ("H",["3"]),
+    ("T",["1"]),
+    ("T^-1",["2"]),
+    ("CNOT",["1","2"]),
+    ]
 
-icm_circuit = Jabalizer.compile(circuit, 3, [""])
+icm_circuit = Jabalizer.compile(circuit, 4, ["H","T","T^-1"])
 
 println(icm_circuit)
 
--- src/test_stim.py
+++ src/test_stim.py
@@ -6,10 +6,12 @@ f = open("./src/circuit")
 
 json_string = f.read()
 
-new_circuit = cirq.read_json(json_text=json_string)
-ops = [cirq.measure(q) for q in new_circuit.all_qubits()]
+f.close()
 
-new_circuit.append(ops)
+new_circuit = cirq.read_json(json_text=json_string)
+
+#ops = [cirq.measure(q) for q in new_circuit.all_qubits()]
+#new_circuit.append(ops)
 
 sampler = stimcirq.StimSampler()
 result = sampler.run(new_circuit, repetitions=30)
