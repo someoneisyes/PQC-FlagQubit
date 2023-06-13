986df8bbae26a2b4cc2d787abaa35ca044949e53
someoneisyes <naratgutz@gmail.com>
2023-06-13 13:37:08 +0300

    the circuit is randomly generated

---
+++ src/random_circuit_generator.py
@@ -0,0 +1,17 @@
+import cirq
+
+line_qubits = []
+
+for i in range(4):
+    line_qubits.append(cirq.LineQubit(i))
+
+print(line_qubits)
+
+
+circuit = cirq.testing.random_circuit(line_qubits,6,0.4,{cirq.H : 1, cirq.T: 1, cirq.CNOT: 2})
+
+f = open("random_circuit", "w")
+
+f.write(cirq.to_json(circuit))
+
+f.close
--- src/test_jabalizer.jl
+++ src/test_jabalizer.jl
@@ -1,14 +1,8 @@
 using Jabalizer
 
-circuit = [
-    ("CNOT",["2","1"]),
-    ("CNOT",["2","3"]),
-    ("CNOT",["2","1"]),
-    ("CNOT",["3","1"]),
-    ("CNOT",["3","4"])
-    ]
+circuit = Jabalizer.load_circuit_from_cirq_json("random_circuit")
 
-icm_circuit = Jabalizer.compile(circuit, 4, [""])
+icm_circuit = Jabalizer.compile(circuit, 4, ["H","S","T"])
 
 println(icm_circuit)
 
